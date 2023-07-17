3db885a6faedb5e9800d66874efc2d378e607fca
mdwalters <mail@mdwalters.ml>
2023-07-17 12:45:21 -0400

    feat: add english translation For some reason, Jed requires an English translation. The English translation will be used as a base for other translations.

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -3,12 +3,18 @@ import Jed from "jed";
 
 export async function i18n_config() {
     const lang = {
+        "en": await fetch(
+                new URL(
+                    "./i18n/en.json",
+                    import.meta.url
+                ).href
+            ).then(res => res.json()),
         "fr": await fetch(
-            new URL(
-                "./i18n/fr.json",
-                import.meta.url
-            ).href
-        ).then(res => res.json())
+                new URL(
+                    "./i18n/fr.json",
+                    import.meta.url
+                ).href
+            ).then(res => res.json())
     };
 
     window.i18n = new Jed({ "locale_data": lang, "domain": "fr" });
